49db3a226ac68fdb2b7234f442e5e38e9cf2884d
Whytebread <196115419+Whytebread@users.noreply.github.com>
2026-07-21 18:36:59 -0500

    added  homepage module to be imported into index and populate upon page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("javascript is running");
+import { homePage } from "./home";
+
+// GOBAL VARIABLES
+const contentDiv = document.querySelector("#content");
+
+// EVENT LISTENER
+document.addEventListener('DOMContentLoaded', homePage);
