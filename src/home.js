ae21844c6945c239dd053f848e7d41db47447723
Whytebread <196115419+Whytebread@users.noreply.github.com>
2026-08-05 13:19:14 -0500

    added css styling and modified menu for consistency

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ export const homePage = () => {
     const contentContainer = document.createElement("div");
     const contentDiv = document.querySelector("#content");
     contentContainer.innerHTML =
-        `        <h1>Teddy's Tapas</h1>
+        `       
         <img src="teddystapas.jpg" alt="Teddy's Tapas">
         <p class="tagline">Where Every Bite is Bear-y Delicious!</p>
         <p>Discover the coziest corner of flavor town at Teddy's Tapas, the charming little restaurant that serves up Spanish-inspired small plates with a big bear hug of warmth and whimsy. Whether you're gathering with friends for a lively evening or treating yourself to a quiet moment of delight, Teddy's turns every meal into a paws-itively unforgettable experience. Our intimate dining room feels like a teddy bear picnic come to life — think soft lighting, playful bear-themed décor, and the irresistible aroma of sizzling garlic, fresh herbs, and golden honey.</p>
