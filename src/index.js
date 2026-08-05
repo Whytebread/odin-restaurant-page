0cb63bd637b914c6730467404dc2c7995881b1a5
Whytebread <196115419+Whytebread@users.noreply.github.com>
2026-08-05 12:01:40 -0500

    added global variables for buttons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { aboutPage } from "./about.js";
 
 // GOBAL VARIABLES
 const contentDiv = document.querySelector("#content");
+const homeButton = document.querySelector("#homeButton");
+const menuButton = document.querySelector("#menuButton");
+const aboutButton = document.querySelector("#aboutButton");
 
 // EVENT LISTENER
 document.addEventListener('DOMContentLoaded', homePage);
